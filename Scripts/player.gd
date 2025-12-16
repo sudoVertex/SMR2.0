3bad6412bdb858de8daa0c0529cba6495f6a210d
#Vertex <sudovertex@protonmail.com>
2025-12-16 19:58:51 +0200

    Added a Prototype testing ground and created the movement logic

---
+++ Scripts/player.gd
@@ -0,0 +1,47 @@
+extends CharacterBody2D
+
+@export var SPEED :int = 150
+@export var JUMP_FORCE :int =  700
+@export var GRAVITY :int = 900
+@export var AnSprite :AnimatedSprite2D
+
+func _physics_process(delta: float) -> void:
+	
+	
+	var direction = Input.get_axis("Left", "Right")
+	
+	if direction !=0:
+		velocity.x = direction * SPEED
+		
+		#if is_on_floor():
+			#$AnSprite.play("Run")
+	else:
+		velocity.x = 0
+		
+		#if is_on_floor():
+			#$AnSprite.play("Idle")
+	
+	#Rotate
+	#if direction == 1:
+		#$AnSprite.flip_h =false
+	#elif direction == -1:
+		#$AnSprite.flip_h =true
+	
+	#Gravity
+	if !is_on_floor():
+		velocity.y += GRAVITY * delta
+		#if velocity.y > 0:
+			#$AnSprite.play("Fall")
+	
+	#Jump
+	
+	if Input.is_action_just_pressed("Jump") and is_on_floor():
+		velocity.y -= JUMP_FORCE
+		
+		
+		#$AnSprite.play("Jump")
+	
+	if Input.is_action_just_released("Jump") and velocity.y <0:
+		velocity.y *= 0.3
+		
+	move_and_slide()
